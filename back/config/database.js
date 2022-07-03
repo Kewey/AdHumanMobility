79b6941ea4a27ecdd5cf7fb53886af0f1c921246
Jordan Souchez <jsouchez@feelity.fr>
2022-07-04 01:28:27 +0200

    💚 add ssl

--- back/config/database.js
+++ back/config/database.js
@@ -10,6 +10,9 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 });
