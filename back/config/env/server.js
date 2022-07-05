8dd743d0248683352f6a2a5cd9fef5f37d909bff
Jordan Souchez <jsouchez@feelity.fr>
2022-07-05 23:36:30 +0200

    🚀 update strapi prod

--- back/config/env/server.js
+++ back/config/env/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env("HEROKU_URL"),
+  url: env("MY_HEROKU_URL"),
+
+  proxy: true,
+
+  app: {
+    keys: env.array("APP_KEYS", ["keys", "keys"]),
+  },
 });
