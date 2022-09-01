73ecb35b2d11b36ab4ab71273122993bedd09534
Jordan Souchez <jsouchez@feelity.fr>
2022-09-01 05:53:37 +0200

    🚀 fix strapi heroku

--- back/config/database.js
+++ back/config/database.js
@@ -10,6 +10,12 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
+    },
+    options: {
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
