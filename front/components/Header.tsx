f0ce2460b88b861186d96ba92ad28b45a2869663
Jordan Souchez <jsouchez@feelity.fr>
2022-10-25 03:19:57 +0200

    ⚰️ Remove GoogleMap

--- front/components/Header.tsx
+++ front/components/Header.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
 import Button from './Button'
-import { SearchGoogleMap } from './form/SearchGoogleMap'
 
 interface HeaderProps<T> {}
 
